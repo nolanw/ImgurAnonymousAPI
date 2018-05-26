888fc6bb70aca04a0fb4433c91a412bbf0ea6564
Nolan Waite <nolan@nolanw.ca>
2018-05-25 23:22:03 -0300

    Start to resize images below 10MB as needed.

--- Sources/ImgurUploader/ImageOperations.swift
+++ Sources/ImgurUploader/ImageOperations.swift
@@ -15,16 +15,69 @@ internal struct ImageFile {
 
 internal enum ImageError: Error {
     case destinationCreationFailed
+    case indeterminateFileSize
     case missingCGImage
     case missingPhotoResource
+    case sourceCreationFailed
 }
+
+// Haven't tested whether they mean 10^6, 2^20, or something else so we'll pick the smallest.
+private let tenMegabytes = 10_000_000
 
 internal final class ResizeImage: AsynchronousOperation<ImageFile> {
     override func execute() throws {
         let tempFolder = try firstDependencyValue(ofType: TemporaryFolder.self)
         let originalImage = try firstDependencyValue(ofType: ImageFile.self)
+        log(.debug, "someone wants to resize \(originalImage) in \(tempFolder)")
 
-        log(.info, "someone wants to resize \(originalImage) in \(tempFolder)")
+        guard let originalByteSize = try originalImage.url.resourceValues(forKeys: [.fileSizeKey]).fileSize else {
+            throw ImageError.indeterminateFileSize
+        }
+
+        if originalByteSize <= tenMegabytes {
+            log(.debug, "original image is within the file size limit so there's nothing to resize")
+            return finish(.success(originalImage))
+        } else {
+            log(.debug, "original image is too large, will need to resize")
+        }
+
+        guard let imageSource = CGImageSourceCreateWithURL(originalImage.url as CFURL, nil) else {
+            throw ImageError.sourceCreationFailed
+        }
+
+        var resizedImageURL = tempFolder.url
+            .appendingPathComponent("resized", isDirectory: false)
+            .appendingPathExtension(originalImage.url.pathExtension)
+
+        // Is kCGImageSourceSubsampleFactor superior to kCGImageSourceThumbnailMaxPixelSize? No idea! Sounds good though so let's try it first. Probably worth testing someday.
+        for factor in [2, 4, 8] {
+            guard
+                let thumbnail = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, [
+                    kCGImageSourceCreateThumbnailFromImageAlways: true,
+                    kCGImageSourceCreateThumbnailWithTransform: true,
+                    kCGImageSourceSubsampleFactor: factor] as NSDictionary),
+                let destination = CGImageDestinationCreateWithURL(resizedImageURL as CFURL, CGImageSourceGetType(imageSource) ?? kUTTypePNG, 1, nil)
+                else { continue }
+
+            CGImageDestinationAddImage(destination, thumbnail, nil)
+            guard CGImageDestinationFinalize(destination) else { break }
+
+            resizedImageURL.removeCachedResourceValue(forKey: .fileSizeKey)
+            guard
+                let resourceValues = try? resizedImageURL.resourceValues(forKeys: [.fileSizeKey]),
+                let byteSize = resourceValues.fileSize
+                else { break }
+
+            if byteSize <= tenMegabytes {
+                log(.debug, "scaled image by a factor of \(factor) is within the file size limit")
+                return finish(.success(ImageFile(url: resizedImageURL)))
+            } else if byteSize > originalByteSize {
+                log(.debug, "subsample factor is producing a larger image, this ain't working")
+                break
+            }
+        }
+
+        log(.error, "subsample failed to produce an image within the file size limit, so it's time do a plain ol' resize using kCGImageSourceThumbnailMaxPixelSize")
 
         throw CocoaError.error(.userCancelled)
     }
